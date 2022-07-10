7045a9382e2c9bdbf4f8af236d4777444c91fc78
Eric Walkingshaw <eric@walkingshaw.net>
2022-07-09 19:15:36 -0600

    Add initial version of big-normal games

--- tft/src/game.rs
+++ tft/src/game.rs
@@ -1,9 +1,12 @@
 //! Defines traits and types used for representing games.
 
+pub(crate) mod big_normal;
 pub(crate) mod extensive;
 pub(crate) mod normal;
 pub(crate) mod simultaneous;
 pub(crate) mod traits;
+
+pub use big_normal::*;
 pub use extensive::*;
 pub use normal::*;
 pub use simultaneous::*;
