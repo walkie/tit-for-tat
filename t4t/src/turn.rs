d63f51ec2ede628b578d1a09182b702e76af8081
Eric Walkingshaw <eric@walkingshaw.net>
2023-11-27 15:56:08 -0800

    Introduce trait for next turn continuation

--- t4t/src/turn.rs
+++ t4t/src/turn.rs
@@ -2,6 +2,16 @@ use std::rc::Rc;
 
 use crate::{Distribution, Game, PerPlayer, PlayerIndex, Profile};
 
+pub trait NextTurn<T, G: Game<P>, const P: usize>:
+    FnOnce(Rc<G::State>, T) -> Turn<G, P> + 'static
+{
+}
+
+impl<F, T, G: Game<P>, const P: usize> NextTurn<T, G, P> for F where
+    F: FnOnce(Rc<G::State>, T) -> Turn<G, P> + 'static
+{
+}
+
 // TODO: Rename to Play or Specification?
 pub struct Turn<G: Game<P>, const P: usize> {
     /// The game state at this turn.
@@ -13,17 +23,15 @@ pub struct Turn<G: Game<P>, const P: usize> {
 /// The next action to performed while playing a game.
 pub enum Action<G: Game<P>, const P: usize> {
     /// One or more players play a move simultaneously.
-    #[allow(clippy::type_complexity)]
     Players {
         to_move: Vec<PlayerIndex<P>>,
-        next: Box<dyn FnOnce(Rc<G::State>, Vec<G::Move>) -> Turn<G, P>>,
+        next: Box<dyn NextTurn<Vec<G::Move>, G, P>>,
     },
 
     /// A move of chance according to the given distribution.
-    #[allow(clippy::type_complexity)]
     Chance {
         distribution: Distribution<G::Move>,
-        next: Box<dyn FnOnce(Rc<G::State>, G::Move) -> Turn<G, P>>,
+        next: Box<dyn NextTurn<G::Move, G, P>>,
     },
 
     /// Award a payoff to the players and terminate the game.
@@ -31,42 +39,28 @@ pub enum Action<G: Game<P>, const P: usize> {
 }
 
 impl<G: Game<P>, const P: usize> Action<G, P> {
-    pub fn player(
-        to_move: PlayerIndex<P>,
-        next: impl FnOnce(Rc<G::State>, G::Move) -> Turn<G, P> + 'static,
-    ) -> Self {
+    pub fn player(to_move: PlayerIndex<P>, next: impl NextTurn<G::Move, G, P>) -> Self {
         Action::players(vec![to_move], move |state, moves| {
             assert_eq!(moves.len(), 1);
             next(state, moves[0])
         })
     }
 
-    pub fn players(
-        to_move: Vec<PlayerIndex<P>>,
-        next: impl FnOnce(Rc<G::State>, Vec<G::Move>) -> Turn<G, P> + 'static,
-    ) -> Self {
+    pub fn players(to_move: Vec<PlayerIndex<P>>, next: impl NextTurn<Vec<G::Move>, G, P>) -> Self {
         Action::Players {
             to_move,
             next: Box::new(next),
         }
     }
 
-    pub fn all_players(
-        next: impl FnOnce(Rc<G::State>, Profile<G::Move, P>) -> Turn<G, P> + 'static,
-    ) -> Self {
-        // Action::AllPlayers {
-        //     next: Box::new(next),
-        // }
+    pub fn all_players(next: impl NextTurn<Profile<G::Move, P>, G, P>) -> Self {
         Action::players(PlayerIndex::all().collect(), move |state, moves| {
             assert_eq!(moves.len(), P);
             next(state, PerPlayer::new(moves.try_into().unwrap()))
         })
     }
 
-    pub fn chance(
-        distribution: Distribution<G::Move>,
-        next: impl FnOnce(Rc<G::State>, G::Move) -> Turn<G, P> + 'static,
-    ) -> Self {
+    pub fn chance(distribution: Distribution<G::Move>, next: impl NextTurn<G::Move, G, P>) -> Self {
         Action::Chance {
             distribution,
             next: Box::new(next),
@@ -86,7 +80,7 @@ impl<G: Game<P>, const P: usize> Turn<G, P> {
     pub fn player(
         state: Rc<G::State>,
         player: PlayerIndex<P>,
-        next: impl FnOnce(Rc<G::State>, G::Move) -> Turn<G, P> + 'static,
+        next: impl NextTurn<G::Move, G, P>,
     ) -> Self {
         Turn::new(state, Action::player(player, next))
     }
@@ -94,14 +88,14 @@ impl<G: Game<P>, const P: usize> Turn<G, P> {
     pub fn players(
         state: Rc<G::State>,
         players: Vec<PlayerIndex<P>>,
-        next: impl FnOnce(Rc<G::State>, Vec<G::Move>) -> Turn<G, P> + 'static,
+        next: impl NextTurn<Vec<G::Move>, G, P>,
     ) -> Self {
         Turn::new(state, Action::players(players, next))
     }
 
     pub fn all_players(
         state: Rc<G::State>,
-        next: impl FnOnce(Rc<G::State>, Profile<G::Move, P>) -> Turn<G, P> + 'static,
+        next: impl NextTurn<Profile<G::Move, P>, G, P>,
     ) -> Self {
         Turn::new(state, Action::all_players(next))
     }
@@ -109,7 +103,7 @@ impl<G: Game<P>, const P: usize> Turn<G, P> {
     pub fn chance(
         state: Rc<G::State>,
         distribution: Distribution<G::Move>,
-        next: impl FnOnce(Rc<G::State>, G::Move) -> Turn<G, P> + 'static,
+        next: impl NextTurn<G::Move, G, P>,
     ) -> Self {
         Turn::new(state, Action::chance(distribution, next))
     }
