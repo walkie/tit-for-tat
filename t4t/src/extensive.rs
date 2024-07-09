297ad47003a4b8b97b094de21aa28ad5a23ebca9
Eric Walkingshaw <eric@walkingshaw.net>
2024-07-08 22:36:39 -0700

    Extensive form WIP

--- t4t/src/extensive.rs
+++ t4t/src/extensive.rs
@@ -1,1 +1,22 @@
-//! Games represented in extensive form.
+use crate::{Distribution, Payoff, PlayerIndex};
+use std::sync::Arc;
+
+type Edge<M, U, const P: usize> = (M, Arc<Extensive<M, U, P>>);
+
+// A game represented in [extensive form](https://en.wikipedia.org/wiki/Extensive-form_game).
+pub enum Extensive<M, U, const P: usize> {
+    Turn {
+        to_move: PlayerIndex<P>,
+        next: Vec<Edge<M, U, P>>,
+    },
+    Chance(Distribution<Edge<M, U, P>>),
+    End(Payoff<U, P>),
+}
+
+/// An [information set](https://en.wikipedia.org/wiki/Information_set_(game_theory)) in an
+/// imperfect information [extensive-form game](https://en.wikipedia.org/wiki/Extensive-form_game).
+pub struct InformationSet<M, const P: usize> {
+    id: usize,
+    player: PlayerIndex<P>,
+    available_moves: Vec<M>,
+}
